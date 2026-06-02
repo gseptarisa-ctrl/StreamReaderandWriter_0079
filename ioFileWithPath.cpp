4e7cf2bd1e44ecaa205f062bddc31c95ae5ede0a
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-06-02 16:27:41 +0700

    membuat tampilannya

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -17,3 +17,5 @@ int main()
     ofstream outfile;
     //menunjuk ke sebuah nama file dengan ekstentsi txt
     outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
